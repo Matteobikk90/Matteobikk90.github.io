3e87cb107d2d4fb8df3b226785cd279533fb0cce
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2015-12-08 17:21:43 +0100

    added project1

---
+++ project1/js/js.js
@@ -0,0 +1,94 @@
+var difficulty;
+
+$(document).ready(function() { 
+
+var playerName = prompt("What's your name, duck hunter?", ".........");
+difficulty = prompt("Choose a skill level 1 - 5");
+
+$("#name").text(playerName);
+
+
+var counter = 0;
+
+$(".duckbtn").click(function() { 
+	counter+= 5;
+    $("#score").html(counter);
+
+});
+
+$(".restart").click(function() {
+	document.location.href="";
+
+});
+
+var count=10;
+
+var secondsLeft=setInterval(timer, 1000); 
+function timer()
+                               {
+  count=count-1;
+
+  if (count < 0)
+                               {
+  
+    $("#boardMC").slideUp()
+    $("#gameOverPage").slideDown()
+     clearInterval(secondsLeft);
+     return;
+}
+
+ document.getElementById("timer").innerHTML=count + " secs";
+
+};
+ 
+});
+
+$(document).ready(function() {
+    animateDiv();
+    
+});
+
+function makeNewPosition() {
+    
+  
+    var h = $(window).height() - 250;
+    var w = $(window).width() - 100;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv() {
+
+    var newq = makeNewPosition();
+    $('.duckbtn').animate({ top: newq[0], left: newq[1] }, 2250 / difficulty, function(){
+      animateDiv();        
+});
+    
+};
+
+   soundManager.setup({
+   url: 'js/soundmanager/swf',
+   flashVersion: 9, 
+   onready: function() {
+   
+var playButton = document.getElementsByClassName("duckbtn");
+ playButton[0].addEventListener("click" , function(event) {
+
+var mySound = soundManager.createSound({
+     "id": 'ducksnd',
+     "url" :"sounds/ducksound.mp3",
+
+});
+
+   mySound.play(); 
+
+})
+
+
+}
+
+});
